8e02d062dbbffd8902a14023bcf1c5105c4d064e
KieuTrang0210 <103953173+KieuTrang0210@users.noreply.github.com>
2023-11-10 01:22:40 +0700

    xu ly lai data

--- data_preprocessing/main.py
+++ data_preprocessing/main.py
@@ -5,38 +5,22 @@ from sklearn.cluster import KMeans
 # đọc file csv
 data = pd.read_csv('Customers.csv')
 
-# loại bỏ cột CustomerID
-data = data.drop(['CustomerID'], axis='columns')
+# loại bỏ cột CustomerID, Gender
+data = data.drop(['CustomerID', 'Gender'], axis='columns')
 
 # Xử lý thuộc tính Profession bị thiếu giá trị
 data['Profession'].fillna(data['Profession'].mode()[0], inplace=True)
 
-# One-hot encoding cho thuộc tính 'Gender'
-d = {"Male": 1, "Female": 0}
-data['Gender'] = data['Gender'].map(d)
-
 # One-hot encoding cho thuộc tính 'Profession'
 data = pd.get_dummies(data, columns=["Profession"], drop_first=True).astype(int)
 
-# Chuẩn hóa thuộc tính 'Annual Income ($)'
-Range = 'Annual Income New ($)'
-data[Range] = 0
-c = 'Annual Income ($)'
-data.loc[((data[c] > 0) & (data[c] <= 20000)), Range] = 1
-data.loc[((data[c] > 20000) & (data[c] <= 40000)), Range] = 2
-data.loc[((data[c] > 40000) & (data[c] <= 60000)), Range] = 3
-data.loc[((data[c] > 60000) & (data[c] <= 80000)), Range] = 4
-data.loc[((data[c] > 80000) & (data[c] <= 100000)), Range] = 5
-data.loc[((data[c] > 100000) & (data[c] <= 120000)), Range] = 6
-data.loc[((data[c] > 120000) & (data[c] <= 140000)), Range] = 7
-data.loc[((data[c] > 140000) & (data[c] <= 160000)), Range] = 8
-data.loc[((data[c] > 160000) & (data[c] <= 180000)), Range] = 9
-data.loc[((data[c] > 180000) & (data[c] <= 200000)), Range] = 10
-data.loc[((data[c] > 200000)), Range] = 11
-data = data.drop(['Annual Income ($)'], axis='columns')
+# Chuẩn hóa Min-Max Scaling cho các thuộc tính
+columns_to_scale = ['Annual Income ($)', 'Spending Score (1-100)', 'Work Experience', 'Family Size']
+data[columns_to_scale] = ((data[columns_to_scale] - data[columns_to_scale].min()) /
+                         (data[columns_to_scale].max() - data[columns_to_scale].min()))
 
 #Lưu dữ liệu đã chuẩn hóa vào một file mới
-data.to_csv("data_preprocessing.csv", index=False)
+# data.to_csv("data_preprocessing.csv", index=False)
 
 #Elbow
 km = KMeans()
