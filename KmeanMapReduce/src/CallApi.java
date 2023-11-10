15d6220220dbf8b2ea7b1ff8c14d3f492a22468e
Nguyễn Đình Tư <nguyendinhtu111022002@gmail.com>
2023-11-10 23:09:18 +0700

    update

---
+++ KmeanMapReduce/src/CallApi.java
@@ -0,0 +1,89 @@
+
+import java.io.*;
+import java.util.Arrays;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.FSDataOutputStream;
+import org.apache.hadoop.fs.FileSystem;
+import org.apache.hadoop.fs.Path;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.util.EntityUtils;
+
+public class CallApi {
+
+    public static void fetchAndWriteDataToHDFS(Configuration conf, String inputFilePath) {
+
+        String apiUrl = "http://127.0.0.1:5000/api/customer";
+
+        try {
+            CloseableHttpClient httpClient = HttpClients.createDefault();
+            HttpGet httpGet = new HttpGet(apiUrl);
+            HttpResponse response = httpClient.execute(httpGet);
+
+            int statusCode = response.getStatusLine().getStatusCode();
+            System.out.println("HTTP Response Code: " + statusCode);
+
+            // lấy dữ liệu từ response
+            String responseBody = EntityUtils.toString(response.getEntity());
+
+            FileSystem hdfs = FileSystem.get(conf);
+            Path filePath = new Path(inputFilePath); // tạo trên java 
+
+            if (hdfs.exists(filePath)) {
+                hdfs.delete(filePath, true);
+                System.out.println("Deleted existing file: " + filePath);
+            }
+            try {
+                FSDataOutputStream dos = hdfs.create(filePath, true);
+                BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(dos));
+                bw.write(responseBody);
+                bw.close();
+                System.out.println("Created file: " + filePath);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            hdfs.close();
+
+            httpClient.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void saveCentroidsToMongoDB(PointWritable[] centroids) {
+        String [] centroidsString = new String[centroids.length];
+        for (int i = 0; i < centroids.length; i++) {
+            centroidsString[i] = '"' + centroids[i].toString() + '"';
+        }
+
+        String apiUrl = "http://127.0.0.1:5000/api/centroids";
+
+        try {
+            CloseableHttpClient httpClient = HttpClients.createDefault();
+            HttpPost httpPost = new HttpPost(apiUrl);
+
+            String jsonBody = Arrays.toString(centroidsString);
+            StringEntity entity = new StringEntity(jsonBody);
+            httpPost.setEntity(entity);
+            httpPost.setHeader("Content-type", "application/json");
+
+            HttpResponse response = httpClient.execute(httpPost);
+
+            int statusCode = response.getStatusLine().getStatusCode();
+            System.out.println("Save Centroids to Mongodb, HTTP Response Code: " + statusCode);
+
+            httpClient.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+
+}
+
---
+++ KmeanMapReduce/src/KCombiner.java
@@ -0,0 +1,45 @@
+
+import org.apache.hadoop.fs.FSDataOutputStream;
+import org.apache.hadoop.fs.FileSystem;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.mapreduce.Reducer;
+
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+
+public class KCombiner extends Reducer<LongWritable, PointWritable, LongWritable, PointWritable> {
+
+    public void reduce(LongWritable centroidId, Iterable<PointWritable> points, Context context)
+            throws IOException, InterruptedException {
+
+        String outputFolder = "/patient-output-points/";
+        String outputFileName = "Cluster" + centroidId.toString() + ".txt";
+
+        FileSystem hdfs = FileSystem.get(context.getConfiguration());
+        FSDataOutputStream dos = hdfs.create(new Path(outputFolder + outputFileName), true);
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(dos));
+
+        bw.write("Cluster" + centroidId + ": ");
+        bw.newLine();
+
+        PointWritable ptSum = PointWritable.copy(points.iterator().next());
+        PointWritable firstPoint = PointWritable.copy(ptSum);
+
+        bw.write(firstPoint.toString());
+        bw.newLine();
+
+        while (points.iterator().hasNext()) {
+            PointWritable currentPoint = points.iterator().next();
+            bw.write(currentPoint.toString());
+            bw.newLine();
+            ptSum.sum(currentPoint);
+        }
+
+        bw.close();
+        hdfs.close();
+
+        context.write(centroidId, ptSum);
+    }
+}
